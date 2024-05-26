dfeeb87448b66aabaeaba3d1725fe928877a0117
CJ <noobclyde21@gmail.com>
2024-05-26 22:26:09 +0800

    idk wtf is this bullshit

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
 const bp = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 app.use(cors())
+app.use(express.json())
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -15,6 +17,47 @@ var connection = mysql.createConnection({
   database : 'inventory-system'
 });
 
+app.get('/users', (req, res) => {
+  connection.query(
+    "SELECT *, CONCAT(fname, ' ', lname) AS name FROM user WHERE status = 'Active'",
+    (error, result) => {
+      if(error){
+        console.log(error);
+      }
+      console.log(results)
+      res.json(result);
+  });
+})
+
+
+app.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.passsword
+
+  // check if the email and password exist
+  const sql = 'SELECT id, fname, lname, email FROM user WHERE status = "active" AND email = ? AND password = ?';
+  connection.query(sql, [email, password], (error, results) => {
+    if (error) {
+      console.error('Error executing SQL query:', error);
+      return res.status(500).json({ error: 'Server error' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // success
+    const userData = {
+      id: results[0].id,
+      email: results[0].email,
+      auth: results[0].authority,
+      fname: results[0].fname,
+      lname: results[0].lname,
+    };
+
+    return res.json(userData);
+  });
+})
 
 app.get('/itemcount' , (req , res) =>{
   const sql = "SELECT c.id , c.name , COUNT(DISTINCT i.id) as count FROM category c LEFT JOIN item i on c.id = i.categoryID  GROUP BY c.id;"
@@ -28,30 +71,30 @@ app.get('/itemcount' , (req , res) =>{
     }
   })
 })
-app.get('/requestno' , (req , res)=>{
-  const sql ='SELECT COUNT(*) as reqNo FROM request WHERE status1 = "Pending";';
+// app.get('/requestno' , (req , res)=>{
+//   const sql ='SELECT COUNT(*) as reqNo FROM request WHERE status1 = "Pending";';
 
-  connection.query(sql ,(err, result) =>{
-    if(err){
-      return res.status(500).json({ error: 'Server error' });
-    }
-    else{
-      return res.status(200).send(result);
-    }  
-  })
-})
-app.get('/requestno-admin' , (req , res)=>{
-  const sql ='SELECT COUNT(*) as reqNo FROM request WHERE status2 = "Pending";';
+//   connection.query(sql ,(err, result) =>{
+//     if(err){
+//       return res.status(500).json({ error: 'Server error' });
+//     }
+//     else{
+//       return res.status(200).send(result);
+//     }  
+//   })
+// })
+// app.get('/requestno-admin' , (req , res)=>{
+//   const sql ='SELECT COUNT(*) as reqNo FROM request WHERE status2 = "Pending";';
 
-  connection.query(sql ,(err, result) =>{
-    if(err){
-      return res.status(500).json({ error: 'Server error' });
-    }
-    else{
-      return res.status(200).send(result);
-    }  
-  })
-})
+//   connection.query(sql ,(err, result) =>{
+//     if(err){
+//       return res.status(500).json({ error: 'Server error' });
+//     }
+//     else{
+//       return res.status(200).send(result);
+//     }  
+//   })
+// })
 
 app.get('/newequipment' , (err ,res) =>{
   const sql = 'SELECT i.asset_code ,i.name , i.serial_no , i.location FROM item i WHERE status = "New";';
@@ -296,72 +339,72 @@ app.post('/asset-update', (req, res) => {
 
 
 
-app.get('/request', async function (req, res) {
+// app.get('/request', async function (req, res) {
    
-    connection.query(
-      'SELECT r.*, CONCAT(u.fname, " ", u.lname) AS requestor, c.name as item_requested FROM `user` u, `request` r, `category` c WHERE r.userID = u.id AND r.categoryID = c.id AND status1 = "Pending" ORDER BY date_requested DESC ;',
-      function (error, results, fields) {
-      if (error) throw error;
-      // console.log(results);
-      res.json(results) //send
-    });
-})
+//     connection.query(
+//       'SELECT r.*, CONCAT(u.fname, " ", u.lname) AS requestor, c.name as item_requested FROM `user` u, `request` r, `category` c WHERE r.userID = u.id AND r.categoryID = c.id AND status1 = "Pending" ORDER BY date_requested DESC ;',
+//       function (error, results, fields) {
+//       if (error) throw error;
+//       // console.log(results);
+//       res.json(results) //send
+//     });
+// })
 
-app.get('/requestTech', async function (req, res) {
+// app.get('/requestTech', async function (req, res) {
    
-  connection.query(
-    'SELECT r.*, CONCAT(u.fname, " ", u.lname) AS requestor, c.name as item_requested FROM `user` u, `request` r, `category` c WHERE r.userID = u.id AND r.categoryID = c.id ORDER BY date_requested DESC ;',
-    function (error, results, fields) {
-    if (error) throw error;
-    // console.log(results);
-    res.json(results) //send
-  });
-})
+//   connection.query(
+//     'SELECT r.*, CONCAT(u.fname, " ", u.lname) AS requestor, c.name as item_requested FROM `user` u, `request` r, `category` c WHERE r.userID = u.id AND r.categoryID = c.id ORDER BY date_requested DESC ;',
+//     function (error, results, fields) {
+//     if (error) throw error;
+//     // console.log(results);
+//     res.json(results) //send
+//   });
+// })
 
-app.get('/requestAdmin', async function (req, res) {
+// app.get('/requestAdmin', async function (req, res) {
    
-  connection.query(
-    'SELECT r.*, CONCAT(u.fname, " ", u.lname) AS requestor, c.name AS item_requested FROM `user` u, `request` r, `category` c WHERE r.userID = u.id AND r.categoryID = c.id AND status2 = "Pending" ORDER BY date_requested DESC ;',
-    function (error, results, fields) {
-    if (error) throw error;
+//   connection.query(
+//     'SELECT r.*, CONCAT(u.fname, " ", u.lname) AS requestor, c.name AS item_requested FROM `user` u, `request` r, `category` c WHERE r.userID = u.id AND r.categoryID = c.id AND status2 = "Pending" ORDER BY date_requested DESC ;',
+//     function (error, results, fields) {
+//     if (error) throw error;
 
-    res.json(results) 
-  });
-})
+//     res.json(results) 
+//   });
+// })
 
-app.post('/request-create', (req, res) => {
-  const item = req.body;
+// app.post('/request-create', (req, res) => {
+//   const item = req.body;
 
-  // connection.query(
-  //   "SELECT COUNT(*) AS stock FROM `item` WHERE categoryID = " + item ,
-  // )
+//   // connection.query(
+//   //   "SELECT COUNT(*) AS stock FROM `item` WHERE categoryID = " + item ,
+//   // )
 
-  connection.query(
-    "INSERT INTO request (name, description, type, quantity, date_requested, date_needed, status1, status2, unit, unit_cost, total_amount, payee, payment_instruction, labor_cost, categoryID, userID) VALUES (?,?,?,?,CURDATE(),?,?,?,?,?,?,?,?,?,?,?)",
-    [
-      item.name,
-      item.description,
-      item.type,
-      item.quantity,
-      item.date_needed,
-      item.status1,
-      item.status2,
-      item.unit,
-      item.unit_cost,
-      item.total_amount,
-      item.payee,
-      item.instruction,
-      item.labor_cost,
-      item.item,
-      item.user_id
-    ],
-    (error, result) => {
-      if (error) {
-        console.log(error);
-      }
-    }
-  );
-});
+//   connection.query(
+//     "INSERT INTO request (name, description, type, quantity, date_requested, date_needed, status1, status2, unit, unit_cost, total_amount, payee, payment_instruction, labor_cost, categoryID, userID) VALUES (?,?,?,?,CURDATE(),?,?,?,?,?,?,?,?,?,?,?)",
+//     [
+//       item.name,
+//       item.description,
+//       item.type,
+//       item.quantity,
+//       item.date_needed,
+//       item.status1,
+//       item.status2,
+//       item.unit,
+//       item.unit_cost,
+//       item.total_amount,
+//       item.payee,
+//       item.instruction,
+//       item.labor_cost,
+//       item.item,
+//       item.user_id
+//     ],
+//     (error, result) => {
+//       if (error) {
+//         console.log(error);
+//       }
+//     }
+//   );
+// });
 
 app.post('/request-status1', (req, res) => {
     const data = req.body;
@@ -389,28 +432,7 @@ app.post('/request-status2', (req, res) => {
   )
 });
 
-app.get('/log', async function (req, res) {
-   
-  connection.query(
-    'SELECT l.*, l.id AS log_id, CONCAT(u.fname, " ", u.lname) AS user_name, i.*, c.name AS type FROM `user` u, `log` l, `item` i, `category` c WHERE l.userID = u.id AND l.itemID = i.id AND i.categoryID = c.id ORDER BY date_done DESC ;',
-    function (error, results, fields) {
-    if (error) throw error;
 
-    res.json(results) 
-  });
-})
-
-app.get('/accounts', async function (req, res){
-  connection.query(
-    "SELECT *, CONCAT(fname, ' ', lname) AS name FROM user WHERE status = 'Active'",
-    (error, result) => {
-    if(error){
-      console.log(error);
-    }
-
-    res.json(result);
-  });
-})
 
 app.post('/user-update', async function (req, res){
   const data = req.body;
